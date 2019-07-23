9661fb6d57cbacdce58f97a23607950c5b753454
Emanuel Righetto <posta@emanuelrighetto.it>
2019-07-23 23:17:22 +0200

    symfony 3

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,6 @@
 require('../scss/app.scss');
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
 
 /**
  * Image preview script
--- config/bundles.php
+++ config/bundles.php
@@ -13,6 +13,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Presta\SitemapBundle\PrestaSitemapBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
